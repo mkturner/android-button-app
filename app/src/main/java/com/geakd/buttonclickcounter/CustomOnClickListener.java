e93ad6865bb5501293ffabee4a186a5f97a5fa7a
Marvin T <contact@marvinkturner.com>
2014-12-10 14:48:10 -0500

    Updates app to show how many times the button was clicked

--- app/src/main/java/com/geakd/buttonclickcounter/CustomOnClickListener.java
+++ app/src/main/java/com/geakd/buttonclickcounter/CustomOnClickListener.java
@@ -5,12 +5,19 @@ import android.view.View;
 public class CustomOnClickListener implements View.OnClickListener{
 
     MainActivity caller;
+    private int count;
 
     public CustomOnClickListener(MainActivity activity){
         this.caller = activity;
+        this.count = 0;
     }
 
     public void onClick (View view){
-        caller.mainTextView.setText("The button got clicked");
+        count++;
+        //convert int to string
+        // String tempResult = integer.toString(count);
+        /* don't need to convert, set text is char sequence which
+        automatically merges numbers and strings */
+        caller.mainTextView.setText("The button has been clicked "+ count +" times");
     }
 }
